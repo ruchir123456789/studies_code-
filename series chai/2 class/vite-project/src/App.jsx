cf4ce042524f6150331c5f17a8b80f2e32f7b7e0
ruchir123456789 <ruchirphotos10@gmail.com>
2025-09-26 12:48:56 +0530

    class 3

---
+++ series chai/2 class/vite-project/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return ( // in react in the main app their should be only one div or "block" thst os exported all the things should be raped inside that 
+    <>
+      <h1>hello ji</h1>
+    </>
+  )
+}
+
+export default App
